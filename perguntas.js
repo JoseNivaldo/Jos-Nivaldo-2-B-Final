cdd089cb0e3a79f9b08613346b9fb5c4c221e492
JoseNivaldo <silva.santos.jose2611@escola.pr.gov.br>
2025-06-12 13:44:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao (
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país é a Russia'
+)
+criaCartao (
+    'Geografia',
+    'Qual o maior país da America do Sul?',
+    'O maior país é o Brasil'
+)
+criaCartao (
+    'Geografia',
+    'Qual o maior país da Africa?',
+    'O maior país é a Argélia'
+)
+criaCartao (
+    'Geografia',
+    'Qual o maior país da Europa?',
+    'O maior país é a Ucrânia'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'Paris'
+)
+
